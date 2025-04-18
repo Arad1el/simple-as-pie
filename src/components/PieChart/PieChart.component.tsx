592f0983d4d5ab1b190b61e80aef5c7fc502295f
Aradiel <25931501+Arad1el@users.noreply.github.com>
2025-04-18 15:33:30 +0100

    Moved to better trig, avoiding the scaling that made the strokes too large

--- src/components/PieChart/PieChart.component.tsx
+++ src/components/PieChart/PieChart.component.tsx
@@ -20,51 +20,50 @@ const PieChart = (props: PieChartProps) => {
     let rotation = -90;
     let segments: JSX.Element[] = [];
 
+    console.log(data);
+
     if (backgroundColour !== undefined) {
         segments.push(<circle className="SimplePieChart-background-segment" key="background"
-            cx="0" cy="0" r="1" fill={backgroundColour} />);
+            cx="0" cy="0" r={radius} fill={backgroundColour} />);
     }
 
-    const getCoordsForPercentage = (percent: number): Coordinates => {
-        const xVal = Math.cos(2 * Math.PI * percent);
-        const yVal = Math.sin(2 * Math.PI * percent);
+    const getCoordsForAngle = (degrees: number): Coordinates => {
+        const radians = degrees  * Math.PI / 180;
 
         return {
-            x: xVal,
-            y: yVal
+            x: Math.cos(radians) * radius,
+            y: Math.sin(radians) * radius
         };
     }
 
-    let startCoords = {
-        x: 1,
-        y: 0
-    };
+    let startAngle = 0;
 
     data.map((dataEntry, index) => {
         if (dataEntry.value === sum) {
             segments.push(<circle className="SimplePieChart-segment" key={index}
-                cx="0" cy="0" r="1" fill={dataEntry.colour}
+                cx="0" cy="0" r={radius} fill={dataEntry.colour}
                 {...dataEntry.events} />);
         }
         else {
             const proportion = (dataEntry.value/sum);
-            const endCoords = getCoordsForPercentage(proportion);
+            const endAngle = (proportion * 360);
+            const endCoords = getCoordsForAngle(endAngle);
             const arcFlag = proportion < 0.5 ? 0 : 1;
             
             segments.push(<path className="SimplePieChart-segment" key={index}
-                d={`M 1 0 A 1 1 0 ${arcFlag} 1 ${endCoords.x} ${endCoords.y} L 0 0`}
-                fill={dataEntry.colour}
-                strokeWidth="1" stroke={dataEntry.colour}
-                style={{transform: `rotate(${rotation}deg)`, transformOrigin: `0 0`}}
-                {...dataEntry.events} />);
+                        d={`M ${radius} ${0} A ${radius} ${radius} 0 ${arcFlag} 1 ${endCoords.x} ${endCoords.y} L 0 0`}
+                        fill={dataEntry.colour}
+                        strokeWidth="1" stroke={dataEntry.colour}
+                        style={{transform: `rotate(${rotation}deg)`, transformOrigin: `0 0`}}
+                        {...dataEntry.events}
+                />);
             
-            startCoords = endCoords;
             rotation += proportion * 360;
         }        
     })
 
     return(
-        <g className="SimplePieChart" style={{scale: radius, translate: `${x}px ${y}px`}}>
+        <g className="SimplePieChart" style={{translate: `${x}px ${y}px`}}>
             {segments.map((segment) => segment)}
         </g>
     )

1f582b0695b1dfaae7df9acf05151625cfad07ca
Aradiel <25931501+Arad1el@users.noreply.github.com>
2025-06-06 06:58:44 +0100

    Events changed to pass DataSet object into the functions

--- src/components/PieChart/PieChart.component.tsx
+++ src/components/PieChart/PieChart.component.tsx
@@ -38,31 +38,33 @@ const PieChart = (props: PieChartProps) => {
         if (dataEntry.value === sum) {
             segments.push(<circle className="SimplePieChart-segment" key={index}
                 cx="0" cy="0" r={radius} fill={dataEntry.colour}
-                onAuxClick={dataEntry.events?.onAuxClick}
-                onBlur={dataEntry.events?.onBlur}
-                onClick={dataEntry.events?.onClick}
-                onDoubleClick={dataEntry.events?.onDoubleClick}
-                onFocus={dataEntry.events?.onFocus}
-                onKeyDown={dataEntry.events?.onKeyDown}
-                onKeyPress={dataEntry.events?.onKeyPress}
-                onKeyUp={dataEntry.events?.onKeyUp}
-                onMouseDown={dataEntry.events?.onMouseDown}
-                onMouseEnter={dataEntry.events?.onMouseEnter}
-                onMouseLeave={dataEntry.events?.onMouseLeave}
-                onMouseMove={dataEntry.events?.onMouseMove}
-                onMouseOut={dataEntry.events?.onMouseOut}
-                onMouseUp={dataEntry.events?.onMouseUp}
-                onPointerCancel={dataEntry.events?.onPointerCancel}
-                onPointerDown={dataEntry.events?.onPointerDown}
-                onPointerEnter={dataEntry.events?.onPointerEnter}
-                onPointerLeave={dataEntry.events?.onPointerLeave}
-                onPointerMove={dataEntry.events?.onPointerMove}
-                onPointerOut={dataEntry.events?.onPointerOut}
-                onPointerUp={dataEntry.events?.onPointerUp}
-                onTouchCancel={dataEntry.events?.onTouchCancel}
-                onTouchEnd={dataEntry.events?.onTouchEnd}
-                onTouchMove={dataEntry.events?.onTouchMove}
-                onTouchStart={dataEntry.events?.onTouchStart} />);
+
+                onAuxClick={(event) => dataEntry.events?.onAuxClick ? dataEntry.events?.onAuxClick(event, dataEntry) : null}
+                onBlur={(event) => dataEntry.events?.onBlur ? dataEntry.events?.onBlur(event, dataEntry) : null}
+                onClick={(event) => dataEntry.events?.onClick ? dataEntry.events?.onClick(event, dataEntry) : null}
+                onDoubleClick={(event) => dataEntry.events?.onDoubleClick ? dataEntry.events?.onDoubleClick(event, dataEntry) : null}
+                onFocus={(event) => dataEntry.events?.onFocus ? dataEntry.events?.onFocus(event, dataEntry) : null}
+                onKeyDown={(event) => dataEntry.events?.onKeyDown ? dataEntry.events?.onKeyDown(event, dataEntry) : null}
+                onKeyPress={(event) => dataEntry.events?.onKeyPress ? dataEntry.events?.onKeyPress(event, dataEntry) : null}
+                onKeyUp={(event) => dataEntry.events?.onKeyUp ? dataEntry.events?.onKeyUp(event, dataEntry) : null}
+                onMouseDown={(event) => dataEntry.events?.onMouseDown ? dataEntry.events?.onMouseDown(event, dataEntry) : null}
+                onMouseEnter={(event) => dataEntry.events?.onMouseEnter ? dataEntry.events?.onMouseEnter(event, dataEntry) : null}
+                onMouseLeave={(event) => dataEntry.events?.onMouseLeave ? dataEntry.events?.onMouseLeave(event, dataEntry) : null}
+                onMouseMove={(event) => dataEntry.events?.onMouseMove ? dataEntry.events?.onMouseMove(event, dataEntry) : null}
+                onMouseOut={(event) => dataEntry.events?.onMouseOut ? dataEntry.events?.onMouseOut(event, dataEntry) : null}
+                onMouseUp={(event) => dataEntry.events?.onMouseUp ? dataEntry.events?.onMouseUp(event, dataEntry) : null}
+                onPointerCancel={(event) => dataEntry.events?.onPointerCancel ? dataEntry.events?.onPointerCancel(event, dataEntry) : null}
+                onPointerDown={(event) => dataEntry.events?.onPointerDown ? dataEntry.events?.onPointerDown(event, dataEntry) : null}
+                onPointerEnter={(event) => dataEntry.events?.onPointerEnter ? dataEntry.events?.onPointerEnter(event, dataEntry) : null}
+                onPointerLeave={(event) => dataEntry.events?.onPointerLeave ? dataEntry.events?.onPointerLeave(event, dataEntry) : null}
+                onPointerMove={(event) => dataEntry.events?.onPointerMove ? dataEntry.events?.onPointerMove(event, dataEntry) : null}
+                onPointerOut={(event) => dataEntry.events?.onPointerOut ? dataEntry.events?.onPointerOut(event, dataEntry) : null}
+                onPointerUp={(event) => dataEntry.events?.onPointerUp ? dataEntry.events?.onPointerUp(event, dataEntry) : null}
+                onTouchCancel={(event) => dataEntry.events?.onTouchCancel ? dataEntry.events?.onTouchCancel(event, dataEntry) : null}
+                onTouchEnd={(event) => dataEntry.events?.onTouchEnd ? dataEntry.events?.onTouchEnd(event, dataEntry) : null}
+                onTouchMove={(event) => dataEntry.events?.onTouchMove ? dataEntry.events?.onTouchMove(event, dataEntry) : null}
+                onTouchStart={(event) => dataEntry.events?.onTouchStart ? dataEntry.events?.onTouchStart(event, dataEntry) : null}
+                />);
         }
         else {
             const proportion = (dataEntry.value/sum);
@@ -75,31 +77,32 @@ const PieChart = (props: PieChartProps) => {
                         fill={dataEntry.colour}
                         strokeWidth="1" stroke={dataEntry.colour}
                         style={{transform: `rotate(${rotation}deg)`, transformOrigin: `0 0`}}
-                        onAuxClick={dataEntry.events?.onAuxClick}
-                        onBlur={dataEntry.events?.onBlur}
-                        onClick={dataEntry.events?.onClick}
-                        onDoubleClick={dataEntry.events?.onDoubleClick}
-                        onFocus={dataEntry.events?.onFocus}
-                        onKeyDown={dataEntry.events?.onKeyDown}
-                        onKeyPress={dataEntry.events?.onKeyPress}
-                        onKeyUp={dataEntry.events?.onKeyUp}
-                        onMouseDown={dataEntry.events?.onMouseDown}
-                        onMouseEnter={dataEntry.events?.onMouseEnter}
-                        onMouseLeave={dataEntry.events?.onMouseLeave}
-                        onMouseMove={dataEntry.events?.onMouseMove}
-                        onMouseOut={dataEntry.events?.onMouseOut}
-                        onMouseUp={dataEntry.events?.onMouseUp}
-                        onPointerCancel={dataEntry.events?.onPointerCancel}
-                        onPointerDown={dataEntry.events?.onPointerDown}
-                        onPointerEnter={dataEntry.events?.onPointerEnter}
-                        onPointerLeave={dataEntry.events?.onPointerLeave}
-                        onPointerMove={dataEntry.events?.onPointerMove}
-                        onPointerOut={dataEntry.events?.onPointerOut}
-                        onPointerUp={dataEntry.events?.onPointerUp}
-                        onTouchCancel={dataEntry.events?.onTouchCancel}
-                        onTouchEnd={dataEntry.events?.onTouchEnd}
-                        onTouchMove={dataEntry.events?.onTouchMove}
-                        onTouchStart={dataEntry.events?.onTouchStart}
+
+                        onAuxClick={(event) => dataEntry.events?.onAuxClick ? dataEntry.events?.onAuxClick(event, dataEntry) : null}
+                        onBlur={(event) => dataEntry.events?.onBlur ? dataEntry.events?.onBlur(event, dataEntry) : null}
+                        onClick={(event) => dataEntry.events?.onClick ? dataEntry.events?.onClick(event, dataEntry) : null}
+                        onDoubleClick={(event) => dataEntry.events?.onDoubleClick ? dataEntry.events?.onDoubleClick(event, dataEntry) : null}
+                        onFocus={(event) => dataEntry.events?.onFocus ? dataEntry.events?.onFocus(event, dataEntry) : null}
+                        onKeyDown={(event) => dataEntry.events?.onKeyDown ? dataEntry.events?.onKeyDown(event, dataEntry) : null}
+                        onKeyPress={(event) => dataEntry.events?.onKeyPress ? dataEntry.events?.onKeyPress(event, dataEntry) : null}
+                        onKeyUp={(event) => dataEntry.events?.onKeyUp ? dataEntry.events?.onKeyUp(event, dataEntry) : null}
+                        onMouseDown={(event) => dataEntry.events?.onMouseDown ? dataEntry.events?.onMouseDown(event, dataEntry) : null}
+                        onMouseEnter={(event) => dataEntry.events?.onMouseEnter ? dataEntry.events?.onMouseEnter(event, dataEntry) : null}
+                        onMouseLeave={(event) => dataEntry.events?.onMouseLeave ? dataEntry.events?.onMouseLeave(event, dataEntry) : null}
+                        onMouseMove={(event) => dataEntry.events?.onMouseMove ? dataEntry.events?.onMouseMove(event, dataEntry) : null}
+                        onMouseOut={(event) => dataEntry.events?.onMouseOut ? dataEntry.events?.onMouseOut(event, dataEntry) : null}
+                        onMouseUp={(event) => dataEntry.events?.onMouseUp ? dataEntry.events?.onMouseUp(event, dataEntry) : null}
+                        onPointerCancel={(event) => dataEntry.events?.onPointerCancel ? dataEntry.events?.onPointerCancel(event, dataEntry) : null}
+                        onPointerDown={(event) => dataEntry.events?.onPointerDown ? dataEntry.events?.onPointerDown(event, dataEntry) : null}
+                        onPointerEnter={(event) => dataEntry.events?.onPointerEnter ? dataEntry.events?.onPointerEnter(event, dataEntry) : null}
+                        onPointerLeave={(event) => dataEntry.events?.onPointerLeave ? dataEntry.events?.onPointerLeave(event, dataEntry) : null}
+                        onPointerMove={(event) => dataEntry.events?.onPointerMove ? dataEntry.events?.onPointerMove(event, dataEntry) : null}
+                        onPointerOut={(event) => dataEntry.events?.onPointerOut ? dataEntry.events?.onPointerOut(event, dataEntry) : null}
+                        onPointerUp={(event) => dataEntry.events?.onPointerUp ? dataEntry.events?.onPointerUp(event, dataEntry) : null}
+                        onTouchCancel={(event) => dataEntry.events?.onTouchCancel ? dataEntry.events?.onTouchCancel(event, dataEntry) : null}
+                        onTouchEnd={(event) => dataEntry.events?.onTouchEnd ? dataEntry.events?.onTouchEnd(event, dataEntry) : null}
+                        onTouchMove={(event) => dataEntry.events?.onTouchMove ? dataEntry.events?.onTouchMove(event, dataEntry) : null}
+                        onTouchStart={(event) => dataEntry.events?.onTouchStart ? dataEntry.events?.onTouchStart(event, dataEntry) : null}
                 />);
             
             rotation += proportion * 360;
--- src/interfaces/DataSet/DataSet.type.tsx
+++ src/interfaces/DataSet/DataSet.type.tsx
@@ -1,33 +1,31 @@
-import { FocusEventHandler, KeyboardEventHandler, MouseEventHandler, PointerEventHandler, TouchEventHandler } from "react";
-
 export interface DataSet {
     colour: string;
     value: number;
     events?: {
-        onAuxClick?: MouseEventHandler;
-        onBlur?: FocusEventHandler;
-        onClick?: MouseEventHandler;
-        onDoubleClick?: MouseEventHandler;
-        onFocus?: FocusEventHandler;
-        onKeyDown?: KeyboardEventHandler;
-        onKeyPress?: KeyboardEventHandler;
-        onKeyUp?: KeyboardEventHandler;
-        onMouseDown?: MouseEventHandler;
-        onMouseEnter?: MouseEventHandler;
-        onMouseLeave?: MouseEventHandler;
-        onMouseMove?: MouseEventHandler;
-        onMouseOut?: MouseEventHandler;
-        onMouseUp?: MouseEventHandler;
-        onPointerCancel?: PointerEventHandler;
-        onPointerDown?: PointerEventHandler;
-        onPointerEnter?: PointerEventHandler;
-        onPointerLeave?: PointerEventHandler;
-        onPointerMove?: PointerEventHandler;
-        onPointerOut?: PointerEventHandler;
-        onPointerUp?: PointerEventHandler;
-        onTouchCancel?: TouchEventHandler;
-        onTouchEnd?: TouchEventHandler;
-        onTouchMove?: TouchEventHandler;
-        onTouchStart?: TouchEventHandler;
+        onAuxClick?(event: React.MouseEvent<SVGPathElement, MouseEvent>, data: DataSet): void;
+        onBlur?(event: React.FocusEvent<SVGPathElement, Element>, data: DataSet): void;
+        onClick?(event: React.MouseEvent<SVGPathElement, MouseEvent>, data: DataSet): void;
+        onDoubleClick?(event: React.MouseEvent<SVGPathElement, MouseEvent>, data: DataSet): void;
+        onFocus?(event: React.FocusEvent<SVGPathElement, Element>, data: DataSet): void;
+        onKeyDown?(event: React.KeyboardEvent<SVGPathElement>, data: DataSet): void;
+        onKeyPress?(event: React.KeyboardEvent<SVGPathElement>, data: DataSet): void;
+        onKeyUp?(event: React.KeyboardEvent<SVGPathElement>, data: DataSet): void;
+        onMouseDown?(event: React.MouseEvent<SVGPathElement, MouseEvent>, data: DataSet): void;
+        onMouseEnter?(event: React.MouseEvent<SVGPathElement, MouseEvent>, data: DataSet): void;
+        onMouseLeave?(event: React.MouseEvent<SVGPathElement, MouseEvent>, data: DataSet): void;
+        onMouseMove?(event: React.MouseEvent<SVGPathElement, MouseEvent>, data: DataSet): void;
+        onMouseOut?(event: React.MouseEvent<SVGPathElement, MouseEvent>, data: DataSet): void;
+        onMouseUp?(event: React.MouseEvent<SVGPathElement, MouseEvent>, data: DataSet): void;
+        onPointerCancel?(event: React.PointerEvent<SVGPathElement>, data: DataSet): void;
+        onPointerDown?(event: React.PointerEvent<SVGPathElement>, data: DataSet): void;
+        onPointerEnter?(event: React.PointerEvent<SVGPathElement>, data: DataSet): void;
+        onPointerLeave?(event: React.PointerEvent<SVGPathElement>, data: DataSet): void;
+        onPointerMove?(event: React.PointerEvent<SVGPathElement>, data: DataSet): void;
+        onPointerOut?(event: React.PointerEvent<SVGPathElement>, data: DataSet): void;
+        onPointerUp?(event: React.PointerEvent<SVGPathElement>, data: DataSet): void;
+        onTouchCancel?(event: React.TouchEvent<SVGPathElement>, data: DataSet): void;
+        onTouchEnd?(event: React.TouchEvent<SVGPathElement>, data: DataSet): void;
+        onTouchMove?(event: React.TouchEvent<SVGPathElement>, data: DataSet): void;
+        onTouchStart?(event: React.TouchEvent<SVGPathElement>, data: DataSet): void;
     }
 }
